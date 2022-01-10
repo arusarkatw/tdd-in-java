c173b2a3262de67279ac644312d0b792eb7c4033
Arusarka Haldar <arusarh@thoughtworks.com>
2022-01-10 12:30:41 +0530

    Aru | updating Readme, dependencies

---
+++ README.md
@@ -0,0 +1,14 @@
+## Prerequisites
+
+- Tested with Java 16 on mac-os. But older Java versions till Java 8 should work
+- Gradle 7.1
+
+## Running
+
+Check you have the setup working by running `./gradlew test` in your terminal on mac-os. You should get something like
+
+![Running tests in terminal](images/running-tests.png)
+
+Then, you can check the appropriate source files and write tests
+
+![files](images/files.png)
--- src/test/java/org/javaexercises/cabby/CabRideTest.java
+++ src/test/java/org/javaexercises/cabby/CabRideTest.java
@@ -6,14 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CabRideTest {
     @Test
-    public void cabFareShouldBe_30_For_3_KmTravelled() {
-        CabRide cabRide = new CabRide(3);
-        assertEquals(30, cabRide.fare());
-    }
-
-    @Test
-    public void cabFareShouldBe_20_For_2_KmTravelled() {
-        CabRide cabRide = new CabRide(2);
-        assertEquals(20, cabRide.fare());
+    public void onePlusTwoShouldBeThree() {
+        assertEquals(3, 1 + 2, "1 plus 2 should have been 3");
     }
 }
